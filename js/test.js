ee4fb5d82c1e3662c010aae6962a3a1398687ef2
2DC <kkts9308@gmail.com>
2022-05-08 23:13:51 +0900

    Practice

--- js/test.js
+++ js/test.js
@@ -1,1 +1,10 @@
-alert("hello world!");
+let admin;
+let name;
+name = "Jone";
+admin = name;
+alert(admin);
+
+let ourPlanetName = "earth";
+let currentUserName = "John";
+alert(ourPlanetName);
+alert(currentUserName);
