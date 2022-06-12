99746a007b7fe5c5f17125fa5d0fa9056403ad1d
2DC <kkts9308@gmail.com>
2022-06-12 11:12:10 +0900

    Update iterator.js

--- iterator.js
+++ iterator.js
@@ -1,14 +1,13 @@
-// 배열은 이터러블 프로토콜을 준수한 이터러블이다.
-const array = ["사과", "복숭아", "참외"];
+for (const i of [1, 2, 3]) {
+  console.log(i);
+}
+console.log();
 
-// Symbol.iterator 메서드는 이터레이터를 반환한다.
-const iterator = array[Symbol.iterator]();
-// Symbol.iterator 메서드가 반환한 이터레이터는 next 메서드를 갖는다.
-console.log("next" in iterator);
-
-// next 메서드를 호출하면 이터러블을 순회하며 순회 결과를 나타내는 이터레이터 리절트 객체를 반환한다.
-// 이터레이터 리절트 객체는 value와 done 프로퍼티를 갖는다.
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
+const iterable = [1, 2, 3];
+const iterator = iterable[Symbol.iterator]();
+for (;;) {
+  const res = iterator.next();
+  if (res.done) break;
+  const i = res.value;
+  console.log(i);
+}
