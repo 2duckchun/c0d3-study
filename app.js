5da71ff1d439f1166d674b3cf2c106faae37414c
2DC <kkts9308@gmail.com>
2022-06-21 17:15:07 +0900

    study

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const http = require('http');
-const routes = require('./routes')
+const express = require('express')
+const path = require('path');
+const bodyParser = require('body-parser')
 
-http.createServer(routes.handler).listen(3000, console.log(routes.justText));
+const app = express();
+
+const adminRouter = require('./routes/admin')
+const shopRouter = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use('/admin', adminRouter);
+app.use(shopRouter);
+
+app.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+// http.createServer(app).listen(3000)을 축약한 것과 같다.
+app.listen(3000, () => {
+    console.log('3000번 포트 실행중');
+})
