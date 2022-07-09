7bbad3c8626ff1fb83c42e8e589ac70d6be79bbf
2DC <kkts9308@gmail.com>
2022-07-09 13:31:45 +0900

    use jest
    study

--- preflight.js
+++ preflight.js
@@ -1,14 +1,14 @@
-const allFuns = {}
+const allFuns = {};
 
-const removeCharX = (str, idx, i = 0, result = '') => {
-    if (i === str.length) {
-      return result
-    }
-    if (i !== idx) {
-      result = result + str[i]
-    }
-    return removeCharX(str, idx, i + 1, result)
+const removeCharX = (str, idx, i = 0, result = "") => {
+  if (str.length === i) {
+    return result;
   }
+  if (i !== idx) {
+    result = result + str[i];
+  }
+  return removeCharX(str, idx, i + 1, result);
+};
 
-allFuns.removeCharX = removeCharX
-module.exports = allFuns
+allFuns.removeCharX = removeCharX;
+module.exports = allFuns;
