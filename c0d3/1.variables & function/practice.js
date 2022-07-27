583ba844ded7d08a708c761ab07cb806158e11fd
2DC <kkts9308@gmail.com>
2022-07-27 23:32:54 +0900

    study
    innerHTML, document.queryselector

--- c0d3/1.variables & function/practice.js
+++ c0d3/1.variables & function/practice.js
@@ -1,25 +1,19 @@
 const baratheon = run_me => {
-    return rum_me() + 5
+    return run_me() + 5
 }
+
 const tyrell = () => {
     return 10
 }
 
-let res = tyrell // function tyrell
+let res = tyrell
 
-res = baratheon(tyrell) // 15
-
+res = baratheon(tyrell)
 res = baratheon(() => {
     return 92
-}, 3) // 97
+}, 3) // 5
 
 res = baratheon(() => {
     const b = 95
     tyrell()
-}) 
-/* res has NaN (Not a Number)
-(undefined + 5 will be shown as NaN, Not a Number)
-BEWARE! This error happens all the time, so be careful
-if a function has no return value, it will return undefined
-no matter what the function inside it returned.
-*/
+})
