d4b4e326dc47bd3673abf9ba084ba88c4565f1b0
2DC <kkts9308@gmail.com>
2022-07-28 20:10:16 +0900

    study
    프로퍼티와 어트리뷰트는 다른 개념이다!

--- c0d3/1.variables & function/defalutparameter.js
+++
@@ -1,8 +0,0 @@
-const add1 = (x = 3, y = 2) => {
-    return x + y + 1
-}
-
-let res = add1() // 6
-
-res = add1(1) + add1() + add1(5,5) // 18
-res = add1(5) === add1(2, 3) + 2 // true
--- c0d3/1.variables & function/recursion-exercise2.js
+++
@@ -1,9 +0,0 @@
-const countTo98 = (count = 0) => {
-    if (count > 98) {
-        return
-    }
-    console.log(count);
-    return countTo98(count + 1)
-}
-
-countTo98()
