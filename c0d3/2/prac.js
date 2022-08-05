07fc20b3c012cea088c762385e6d4722f8d00edd
2DC <kkts9308@gmail.com>
2022-08-05 23:03:02 +0900

    exercise
    클로저UI, 소수생성기, 문자열브레이커, 헬로제너레이터 등등 문제를 보고 답지없이 직접 풀어본 뒤, 코딩을 점검하는 시간을 가졌음.

--- c0d3/2/prac.js
+++ c0d3/2/prac.js
@@ -1,35 +1,11 @@
-const isPrime = (num, i = 2) => {
-    if (num < i) return false
-    if (num === i) return true
-    if (num % i === 0) return false
-    return isPrime(num, i + 1)
-}
-
-const getNextPrime = (start) => {
-    if (isPrime(start)) {
-        return start
+const makeHello = (num, i = 1, result = "") => {
+    if (num < i) return result
+    if (i % 5 === 0) {
+        result = result + "hello " + "</br>"
+        return makeHello(num, i + 1, result)
     }
-    return getNextPrime(start + 1)
-}
-
-const primeMachine = start => {
-    if (start < 2) {
-        start = 1
+    return makeHello(num, i + 1, result + "hello ")
     }
-    if (isPrime(start)) {
-        start = start + 1
-    }
-    return () => {
-    const result = getNextPrime(start)
-    start = result + 1
-    return result
-    }
-}
 
-const getPrime = primeMachine(2)
-
-console.log(getPrime());
-console.log(getPrime());
-console.log(getPrime());
-console.log(getPrime());
-console.log(getPrime());
+    const b = makeHello(11)
+    console.log(b);
