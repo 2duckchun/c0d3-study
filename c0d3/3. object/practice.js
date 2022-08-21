6f3b070f4ce13f2d4e7b4d844be2b8e3c5999da2
2DC <kkts9308@gmail.com>
2022-08-21 23:15:51 +0900

    휴식
    find에 대해서, this 용법에 대해서 복습해볼 필요성을 느낌.

--- c0d3/3. object/practice.js
+++ c0d3/3. object/practice.js
@@ -4,3 +4,12 @@ const mergeArrays = (arr1, arr2) => {
 
 const a = mergeArrays(["1", "2"], ["3", "4", "5"]);
 console.log(a);
+
+const firstLongerThan = (arr, num) => {
+  return arr.find((e) => {
+    return e.length > 5;
+  });
+};
+
+const b = firstLongerThan(["Ekans", "Arbok", "Pikachu", "Raichu"], 5);
+console.log(b);
