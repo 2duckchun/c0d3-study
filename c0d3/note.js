dcdb8778f01da1b8fd6702986ffb725dd43c697c
2DC <kkts9308@gmail.com>
2022-10-27 23:13:07 +0900

    HTML 이벤트에 대한 공부
    click 이벤트에 createElement와 클로저를 적용해보았음.
    text를 Math.random으로 암호화하는 texthiding 이라는 것을 제작해보았음.

--- c0d3/note.js
+++ c0d3/note.js
@@ -2,18 +2,9 @@
 Write a function that takes in an array of numbers and a number, and returns true if any pairs add up to the number. (The numbers in the array is not unique, meaning there may be duplicate numbers)
 */
 
-const solution = (arr, num) => {
-    let answer = false
-    const map = new Map()
-    arr.forEach((e) => {
-        if (!map.has(num - e)) {
-            map.set(e, true)
-        } else answer = true
-    })
-    return answer
+const solution = (num1, num2) => {
+    let sum = num + num2;
+    return sum
 }
 
-
-
-
-const b = solution([1,2,22,333,23], 8888)
+console.log(solution(5, 8));
