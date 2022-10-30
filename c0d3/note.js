68cebccba95eaf39e227e77973da2c6e446b707e
2DC <kkts9308@gmail.com>
2022-10-30 11:25:55 +0900

    노트북에서 할 공부 옮기기
    노트북에서 할 공부 옮기기

--- c0d3/note.js
+++ c0d3/note.js
@@ -1,10 +1,8 @@
-/*
-Write a function that takes in an array of numbers and a number, and returns true if any pairs add up to the number. (The numbers in the array is not unique, meaning there may be duplicate numbers)
-*/
-
-const solution = (num1, num2) => {
-    let sum = num + num2;
-    return sum
+const async = (msg, second) => {
+    setTimeout(() => {
+        console.log(msg);
+    }, second * 1000);
 }
 
-console.log(solution(5, 8));
+async('슬기님 파이팅', 2)
+console.log('나도 파이팅');
