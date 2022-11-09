aa3adf05aa968111e5257b036f71de20ee804e82
2DC <kkts9308@gmail.com>
2022-11-09 21:24:27 +0900

    라이브러리 실습
    각 종 라이브러리를 다운받아 실습함

---
+++ c0d3/js4/External-JS-Libraries.js/external-JS-libraries.js
@@ -0,0 +1,9 @@
+/* You can import JavaScript libraries that other people have written into your HTML. 
+To use someone else's JavaScript file, 
+just give your script tag a src attribute with the URL value so the browser can send a request to get the file. 
+
+다른 사람이 쓴 자바스크립트 라이브러리를 HTML에 import할 수 있다.
+다른 사람의 자바스크립트 file을 쓰기 위해서는
+스크립트 태그에 src 어트리뷰트를 URL값과 함께 주면 된다. 그럼 브라우저가 해당 file을 get하기 위해
+request를 보낼 것이다.
+*/
