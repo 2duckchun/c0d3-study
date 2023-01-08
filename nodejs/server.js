dbeb97c5616971debe274279467d78170c6c3db4
2DC <kkts9308@gmail.com>
2023-01-08 22:32:10 +0900

    http 모듈 response 실습

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req)
-    process.exit()
+    // console.log(req)
+    console.log(req.url, req.method, req.headers)
+    // process.exit()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<meta charset="utf-8">')
+    res.write('<head><title>hello.</title></head>')
+    res.write('<body><h1>한글</h1></body>')
+    res.write('</meta>')
+    console.log(res)
 })
 
 server.listen(8080)
